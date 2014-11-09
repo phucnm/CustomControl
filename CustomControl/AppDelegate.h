95c08d9559354d8c3c88b25dfe33ff2f3fae614c
phucnguyenhcmus <phucnguyen>
2014-11-09 18:43:32 +0700

    Initial Commit

---
+++ CustomControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomControl
+//
+//  Created by Phuc Nguyen on 11/9/14.
+//  Copyright (c) 2014 PHUCNGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomControl
+//
+//  Created by Phuc Nguyen on 11/9/14.
+//  Copyright (c) 2014 PHUCNGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
